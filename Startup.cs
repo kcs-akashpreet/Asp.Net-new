d65e3251e395c4bef90c9ec0c4a6f36b6a6455d6
kcs-akashpreet <59723027+kcs-akashpreet@users.noreply.github.com>
2020-04-28 14:15:30 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ namespace HelloWeb
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello KCS1");
+                return context.Response.WriteAsync("Hello KCS2");
             });
         }
     }
